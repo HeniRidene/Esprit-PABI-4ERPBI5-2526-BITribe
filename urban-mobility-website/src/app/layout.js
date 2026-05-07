598d79c9ccf639900481b8ac239495274240147e
HeniRidene <heniridene77@gmail.com>
2026-05-07 14:41:59 +0100

    small fix

--- urban-mobility-website/src/app/layout.js
+++ urban-mobility-website/src/app/layout.js
@@ -16,7 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${inter.variable} h-full`}>
-      <body className="min-h-full" style={{ fontFamily: "'Inter', sans-serif" }}>
+      <body className="min-h-full" style={{ fontFamily: "'Inter', sans-serif" }} suppressHydrationWarning>
         {children}
       </body>
     </html>
